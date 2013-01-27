13fa1adbabc941ef368adcfdf1279ed973c5fd43
Tobias Koenig <tobias.koenig@kdab.com>
2013-01-27 16:48:00 +0100

    Add API documentation to FilterProxyDataModel

--- filterproxydatamodel/tests/filterproxydatamodeltest/src/FilterProxyDataModel.hpp
+++ filterproxydatamodel/tests/filterproxydatamodeltest/src/FilterProxyDataModel.hpp
@@ -3,15 +3,41 @@
 
 #include <bb/cascades/DataModel>
 
+/**
+ * @short A filter proxy model for Cascades data model classes.
+ *
+ * The FilterProxyDataModel can be put between a bb::cascades::DataModel
+ * and a ListView to filter out certain items, so that the ListView only
+ * shows a subset of the original entries from the source data model.
+ *
+ * To use the FilterProxyDataModel, inherit from it and reimplement the
+ * acceptItem() method.
+ */
 class FilterProxyDataModel : public bb::cascades::DataModel
 {
     Q_OBJECT
 
 public:
+    /**
+     * Creates a new FilterProxyDataModel object
+     *
+     * @param parent The parent object.
+     */
     FilterProxyDataModel(QObject *parent = 0);
+
+    /**
+     * Destroys the FilterProxyDataModel.
+     */
     ~FilterProxyDataModel();
 
-    void setSourceModel(bb::cascades::DataModel *sourceModel);
+    /**
+     * Sets the source @p model the filter model should work on.
+     */
+    void setSourceModel(bb::cascades::DataModel *model);
+
+    /**
+     * Returns the source model the filter model works on.
+     */
     bb::cascades::DataModel* sourceModel() const;
 
     // @reimplemented from bb::cascades::DataModel
@@ -20,12 +46,15 @@ public:
     QString itemType(const QVariantList &indexPath);
     QVariant data(const QVariantList &indexPath);
 
-Q_SIGNALS:
-    void hideEmptyGroupsChanged();
-
 protected:
+    /**
+     * This method must be reimplemented to define the filter criterion.
+     */
     virtual bool acceptItem(const QVariantList &indexPath, const QString &itemType, const QVariant &data) const;
 
+    /**
+     * This method must be called after the filter criterion has changed.
+     */
     void invalidateFilter();
 
 private:
